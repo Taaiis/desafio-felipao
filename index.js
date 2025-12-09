5a8f6f9f2da0da353b4b9638453c81b13fdf23d1
Taís Pereira <taispereira.pessoal@gmail.com>
2025-12-08 23:42:18 -0300

    simplificando o codigo

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ let nomeHeroi = "Homem de ferro"
 let nivelHeroi = 9500;
 let nivel = " ";
 
-// definindo o nível do herói, ultilizando a estrutua de decisão if...else if...else
+// definindo o nível do herói, ultilizando a estrutua de decisão 
  if (nivelHeroi <= 1000) {
         nivel = "Ferro";
-}  else if (nivelHeroi >= 1001 && nivelHeroi <= 2000) {
-         nivel = "bronze";
-} else if (nivelHeroi >= 2001 && nivelHeroi <= 5000) {
-        nivel = "prata";
-} else if (nivelHeroi >= 5001 && nivelHeroi <= 7000) {
-        nivel = "ouro";
-} else if (nivelHeroi >= 7001 && nivelHeroi <= 8000) {
+}  else if (nivelHeroi <= 2000) {
+         nivel = "Bronze";
+} else if (nivelHeroi <= 5000) {
+        nivel = "Prata";
+} else if (nivelHeroi <= 7000) {
+        nivel = "Ouro";
+} else if (nivelHeroi <= 8000) {
        nivel = "Platina";
-} else if (nivelHeroi > 8001 && nivelHeroi <= 9000) {
+} else if (nivelHeroi <= 9000) {
         nivel = "Ascendente";
- } else if (nivelHeroi >= 9001 && nivelHeroi <= 10000){
+ } else if (nivelHeroi <= 10000){
        nivel = "Imortal"; 
  } else if (nivelHeroi > 10000) {
        nivel = "Radiante";
  }
 //saída no console com o nome do herói e seu nível
-        console.log(`O Herói de nome ${nomeHeroi} esta no nível de ${nivel}`);
+        console.log(`O Herói de nome ${nomeHeroi} esta no nível ${nivel}`);
 
     
